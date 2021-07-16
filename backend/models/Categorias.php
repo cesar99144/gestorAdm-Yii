71353c1381cb27f311f7c2391c608b8d10ff5ffb
César Ferreira <cesarferreiraa67@gmail.com>
2021-07-15 23:25:17 -0300

    Categorias models e controller

---
+++ backend/models/Categorias.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "categorias".
+ *
+ * @property int $id
+ * @property string $nome
+ *
+ * @property Produtos[] $produtos
+ */
+class Categorias extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'categorias';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome'], 'required'],
+            [['nome'], 'string', 'max' => 80],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+        ];
+    }
+
+    /**
+     * Gets query for [[Produtos]].
+     *
+     * @return \yii\db\ActiveQuery|ProdutosQuery
+     */
+    public function getProdutos()
+    {
+        return $this->hasMany(Produtos::className(), ['idcategoria' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return CategoriasQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CategoriasQuery(get_called_class());
+    }
+}
